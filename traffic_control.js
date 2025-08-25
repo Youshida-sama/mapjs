3f42fa6fd5ae2401fcdbbe4d6c68196d604f68ca
akari <koshelev.dima.1999@yandex.ru>
2025-08-25 21:41:42 +0300

    add files

---
+++ traffic_control.js
@@ -0,0 +1,33 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [45.054, 38.980],
+        zoom: 10,
+        controls: ["zoomControl"],
+    });
+
+    // Создадим элемент управления "Пробки".
+    var trafficControl = new ymaps.control.TrafficControl({
+        state: {
+            // Отображаются пробки "Сейчас".
+            providerKey: "traffic#actual",
+            // Начинаем сразу показывать пробки на карте.
+            trafficShown: true,
+        },
+    });
+    // Добавим контрол на карту.
+    myMap.controls.add(trafficControl);
+    // Получим ссылку на провайдер пробок "Сейчас" и включим показ инфоточек.
+    trafficControl
+        .getProvider("traffic#actual")
+        .state.set("infoLayerShown", true);
+
+    var data = ymaps.geoXml.load("geoObjects.kml");
+
+    // Обработка полученного асинхронно ответа.
+    data.then(function(res) {
+        // Добавление объектов на карту.
+        myMap.geoObjects.add(res.geoObjects);
+    });
+}
