f47cdad782c0fbfe3a5c665e22221d1369284128
akari <koshelev.dima.1999@yandex.ru>
2025-08-25 21:53:27 +0300

    add files

--- traffic_control.js
+++ traffic_control.js
@@ -23,7 +23,7 @@ function init() {
         .getProvider("traffic#actual")
         .state.set("infoLayerShown", true);
 
-    var data = ymaps.geoXml.load("https://raw.githubusercontent.com/Youshida-sama/mapjs/refs/heads/main/geoObjects.kml");
+    var data = ymaps.geoXml.load("https://raw.githubusercontent.com/Youshida-sama/mapjs/refs/heads/main/geoObjects_2.kml");
 
     // Обработка полученного асинхронно ответа.
     data.then(function(res) {
